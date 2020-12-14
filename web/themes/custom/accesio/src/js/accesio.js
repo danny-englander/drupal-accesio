bb380a9443df9e8c587df4571e864064fb3cc186
Danny Englander <dannyenglander311@gmail.com>
2020-12-14 05:16:37 -0800

    Theming video hero

--- web/themes/custom/accesio/src/js/accesio.js
+++ web/themes/custom/accesio/src/js/accesio.js
@@ -10,7 +10,6 @@
     attach: function (context, settings) {
       // Instantiate an accordion.
       if ($().accordion) {
-        console.log('foo');
         $("[id^=js-accordion-id]").once('js-accordion').each(function () {
           $(this).accordion({
             buttonsGeneratedContent: "html",
@@ -20,4 +19,51 @@
     },
   };
 
+  /*
+* YTPlayer.
+ */
+  Drupal.behaviors.accesioYTPlayer = {
+    attach: function (context, settings) {
+      // Youtube video background.
+      if ($().YTPlayer) {
+        video_url = $("#video-url");
+        $(context).find(video_url).once("video-vimeo-bg").each(function () {
+          // Instantiate the player.
+          $(video_url).YTPlayer();
+          // Add custom A11y player controls.
+          $.YTPlayer.controls.play = "<span role='button' tabindex='0'>" + Drupal.t('Play') + "</span>";
+          $.YTPlayer.controls.pause = "<span role='button' tabindex='0'>" + Drupal.t('Pause') + "</span>";
+          // Detect when the video has started playing.
+          $(video_url).on("YTPStart", function (e) {
+            $('#video-bg-container').addClass('is-started');
+            // console.log('started');
+          });
+        });
+      }
+    },
+  };
+
+  /*
+* vimeo_player.
+*/
+  Drupal.behaviors.accesioYTPlayer = {
+    attach: function (context, settings) {
+      // Vimeo video background.
+      if ($().vimeo_player) {
+        video_url = $("#video-url");
+        $(context).find(video_url).once("video-yt-bg").each(function () {
+          // Instantiate the player.
+          $(video_url).vimeo_player();
+          // Add custom A11y player controls.
+          $.vimeo_player.controls.play = "<span role='button' tabindex='0'>" + Drupal.t('Play') + "</span>";
+          $.vimeo_player.controls.pause = "<span role='button' tabindex='0'>" + Drupal.t('Pause') + "</span>";
+          // Detect when the video has started playing.
+          $(video_url).on("VPStart", function (e) {
+            $('#video-bg-container').addClass('is-started');
+            // console.log('started');
+          });
+        });
+      }
+    },
+  };
 }(Drupal, jQuery));
